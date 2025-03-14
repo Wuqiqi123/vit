e318ef5918623142871b97b2782bbfe02c906647
wqq <1258552199@qq.com>
2025-03-14 22:31:34 +0800

    udpate

--- vit/model/vit.py
+++ vit/model/vit.py
@@ -1,35 +1,7 @@
 import torch
 import torch.nn as nn
 from einops import rearrange, repeat
-
-
-class Attention(nn.Module):
-    def __init__(self, embed_dim, num_heads, dropout=0.0):
-        super(Attention, self).__init__()
-        self.embed_dim = embed_dim
-        self.num_heads = num_heads
-        self.head_dim = embed_dim // num_heads
-        self.scale = self.head_dim ** -0.5
-
-        self.qkv = nn.Linear(embed_dim, embed_dim * 3)
-        self.fc = nn.Linear(embed_dim, embed_dim)
-        self.dropout = nn.Dropout(dropout)
-        self._init_weights()
-
-    def _init_weights(self):
-        nn.init.xavier_uniform_(self.qkv.weight)
-        nn.init.xavier_uniform_(self.fc.weight)
-
-    def forward(self, x):
-        qkv = self.qkv(x).chunk(3, dim=-1)
-        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.num_heads), qkv)
-        
-        attn = torch.einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
-        attn = torch.softmax(attn, dim=-1)
-        out = torch.einsum('b h i j, b h j d -> b h i d', attn, v)
-        out = rearrange(out, 'b h n d -> b n (h d)')
-        out = self.fc(out)
-        return out, attn
+import math
     
 class FeedForward(nn.Module):
     def __init__(self, dim, hidden_dim, dropout=0.0):
@@ -58,13 +30,13 @@ class AttentionBlock(nn.Module):
         """
         super().__init__()
         self.layer_norm_1 = nn.LayerNorm(embed_dim)
-        self.attn = Attention(embed_dim, num_heads)
+        self.attn = nn.MultiheadAttention(embed_dim, num_heads, dropout=dropout)
         self.layer_norm_2 = nn.LayerNorm(embed_dim)
         self.feed_forword = FeedForward(embed_dim, hidden_dim, dropout=dropout)
 
     def forward(self, x):
         inp_x = self.layer_norm_1(x)
-        x = x + self.attn(inp_x)[0]
+        x = x + self.attn(inp_x, inp_x, inp_x)[0]
         x = x + self.feed_forword(self.layer_norm_2(x))
         return x
 
